7670ec41fdc4144e7c2c8d6786b819c2cad24354
Kaoliangkid <gabriel@landpact.com>
2025-07-02 23:01:41 -0600

    Update follow.js

--- modes/follow.js
+++ modes/follow.js
@@ -3,7 +3,7 @@ const alpha = 0.8;
 export default {
   name: 'Follow',
   settings: [
-    { key:'threshold', label:'Threshold (ft/s²)', type:'range',    min:0,  max:10,   step:0.1, default:1   },
+    { key:'threshold', label:'Threshold 2(ft/s²)', type:'range',    min:0,  max:10,   step:0.1, default:1   },
     { key:'maxMag',    label:'Max Motion (ft/s²)', type:'range',    min:10, max:60,   step:1,   default:40  },
     { key:'maxDur',    label:'Max Duration (ms)',  type:'range',    min:100,max:1000, step:10,  default:800 },
     { key:'echo',      label:'Echo',                type:'checkbox',               default:true},
